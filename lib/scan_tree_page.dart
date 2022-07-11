3aac8a7562e62409257cd91c88d8aac843251146
Matteo <matteo.violani@studio.unibo.it>
2022-07-11 12:27:09 +0200

    added scan qr page

--- lib/scan_tree_page.dart
+++ lib/scan_tree_page.dart
@@ -1,22 +1,39 @@
+import 'dart:io';
 import 'package:ar_flutter_plugin/managers/ar_location_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_session_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_object_manager.dart';
 import 'package:ar_flutter_plugin/managers/ar_anchor_manager.dart';
 import 'package:ar_flutter_plugin/datatypes/config_planedetection.dart';
 import 'package:ar_flutter_plugin/ar_flutter_plugin.dart';
+import 'package:ar_flutter_plugin/models/ar_hittest_result.dart';
+import 'package:ar_flutter_plugin/datatypes/node_types.dart';
+import 'package:ar_flutter_plugin/datatypes/hittest_result_types.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
+import 'package:vector_math/vector_math_64.dart';
+import 'dart:math';
+import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:ar_flutter_plugin/models/ar_anchor.dart';
 import 'package:tree_ar/constant_vars.dart';
 import 'package:flutter/material.dart';
 
-class ScanTreePage extends StatelessWidget {
+class ScanTreePage extends StatefulWidget {
   const ScanTreePage({Key? key}) : super(key: key);
+
+  @override
+  State<ScanTreePage> createState() => _ScanTreePageState();
+}
+
+class _ScanTreePageState extends State<ScanTreePage> {
+  ScanQRView scanPage = const ScanQRView();
+  ARWidget arPage = const ARWidget();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
       child: Stack(children: [
-        const ARWidget(),
+        const ScanQRView(),
         Padding(
           padding: pagePadding,
           child: Row(
@@ -39,6 +56,83 @@ class ScanTreePage extends StatelessWidget {
   }
 }
 
+class ScanQRView extends StatefulWidget {
+  const ScanQRView({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ScanQRViewState();
+}
+
+class _ScanQRViewState extends State<ScanQRView> {
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  bool stopScanning = false;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildQrView(context),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: mainColor,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 20),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        if (true) {
+          controller.pauseCamera();
+          //then show ar view and bottom banner info
+          result = scanData;
+        }
+      });
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
+
+//________________AR______ZONE____________
+
 class ARWidget extends StatefulWidget {
   const ARWidget({Key? key}) : super(key: key);
 
@@ -49,6 +143,10 @@ class ARWidget extends StatefulWidget {
 class _ARWidgetState extends State<ARWidget> {
   late ARSessionManager arSessionManager;
   late ARObjectManager arObjectManager;
+  late ARAnchorManager arAnchorManager;
+
+  List<ARNode> nodes = [];
+  List<ARAnchor> anchors = [];
 
   @override
   void dispose() {
@@ -71,13 +169,33 @@ class _ARWidgetState extends State<ARWidget> {
       ARLocationManager arLocationManager) {
     this.arSessionManager = arSessionManager;
     this.arObjectManager = arObjectManager;
+    this.arAnchorManager = arAnchorManager;
 
     this.arSessionManager.onInitialize(
-        showAnimatedGuide: true,
-        handleTaps: true,
+        showAnimatedGuide: false,
+        handlePans: true,
         showFeaturePoints: true,
-        showPlanes: false,
+        handleRotation: true,
         showWorldOrigin: false);
     this.arObjectManager.onInitialize();
+    this.arSessionManager.onPlaneOrPointTap = onPlaneOrPointTapped;
+  }
+
+  void onPlaneOrPointTapped(List<ARHitTestResult> hitTestResults) {
+    var singleHitTestResult = hitTestResults.firstWhere(
+        (hitTestResult) => hitTestResult.type == ARHitTestResultType.plane);
+    var newAnchor =
+        ARPlaneAnchor(transformation: singleHitTestResult.worldTransform);
+    arAnchorManager.addAnchor(newAnchor);
+    anchors.add(newAnchor);
+    // Add note to anchor
+    var newNode = ARNode(
+        type: NodeType.webGLB,
+        uri:
+            "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF-Binary/Duck.glb",
+        scale: Vector3(0.5, 0.5, 0.5),
+        position: Vector3(0.0, 0.0, 0.0),
+        rotation: Vector4(1.0, 0.0, 0.0, 0.0));
+    arObjectManager.addNode(newNode, planeAnchor: newAnchor);
   }
 }
