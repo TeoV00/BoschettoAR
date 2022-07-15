2aa710a5e5b092651a71751ba7cdcadc9fc7c2ed
Matteo <matteo.violani@studio.unibo.it>
2022-07-16 00:38:15 +0200

    from/to map conversion Tree obj

--- lib/Database/dataModel.dart
+++ lib/Database/dataModel.dart
@@ -5,12 +5,37 @@ class Tree {
   final String name;
   final String descr;
   final int height;
+  final int diameter;
+  final int co2;
 
   Tree(
       {required this.id,
       required this.name,
       required this.descr,
-      required this.height});
+      required this.height,
+      required this.diameter,
+      required this.co2});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'descr': descr,
+      'height': height,
+      'diameter': diameter,
+      'co2': co2,
+    };
+  }
+
+  factory Tree.fromMap(Map<String, dynamic> treeDb) {
+    return Tree(
+        id: treeDb['id'],
+        name: treeDb['name'],
+        descr: treeDb['descr'],
+        height: treeDb['height'],
+        diameter: treeDb['diameter'],
+        co2: treeDb['co2']);
+  }
 }
 
 class Project {
@@ -19,4 +44,10 @@ class Project {
   final String descr;
 
   Project({required this.projectId, required this.name, required this.descr});
+}
+
+class Badge {
+  final int id;
+  final String descr;
+  Badge({required this.id, required this.descr});
 }
