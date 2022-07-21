5b7e9870712deb77220ae1d9eb901be55e4d16b6
Matteo <matteo.violani@studio.unibo.it>
2022-07-21 18:05:24 +0200

    added creation query of tables

--- lib/Database/database_constant.dart
+++ lib/Database/database_constant.dart
@@ -12,10 +12,51 @@ const treeTable = "Tree";
 
 var defaultUser = User(
   userId: DEFAULT_USER_ID,
-  name: "Nome",
+  name: "Default",
   surname: "cognome",
   dateBirth: "Data di nascita",
   course: "Corso universitario",
   registrationDate: "data Immatric.",
   userImageName: "userPlaceholder.jpeg",
 );
+
+//list of SQL query for creation of tables in db
+List<String> creationQuery = [
+  //0
+  '''CREATE TABLE UserProfile (
+     userId INTEGER PRIMARY KEY,
+     name TEXT,
+     surname TEXT,
+     dateBirth TEXT ,
+     course TEXT,
+     registrationDate TEXT,
+     userImageName TEXT );''',
+  //1
+  '''CREATE TABLE Project (
+     name TEXT,
+     projectId INTEGER PRIMARY KEY,
+     treeId INTEGER,
+     descr TEXT,
+     link TEXT );''',
+  //2
+  '''CREATE TABLE Badge (
+     id INTEGER PRIMARY KEY,
+     descr TEXT,
+     imageName TEXT);''',
+  //3
+  '''CREATE TABLE UserTrees (
+     userId INTEGER,
+     treeId INTEGER);''',
+  //4
+  '''CREATE TABLE UserBadge (
+     idBadge INTEGER,
+     userId INTEGER );''',
+  //5
+  '''CREATE TABLE Tree (
+     treeId INTEGER PRIMARY KEY,
+     name TEXT,
+     descr TEXT,
+     height INTEGER,
+     diameter INTEGER,
+     co2 INTEGER );''',
+];
