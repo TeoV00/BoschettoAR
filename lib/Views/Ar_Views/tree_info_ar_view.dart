bfb5f26ea287a2c2fa0511282b4d3d7a1ded176f
Matteo <matteo.violani@studio.unibo.it>
2022-08-01 18:47:45 +0200

    show proportional icons to value

--- lib/Views/Ar_Views/tree_info_ar_view.dart
+++ lib/Views/Ar_Views/tree_info_ar_view.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:tree_ar/Database/dataModel.dart';
 import 'package:tree_ar/Views/Ar_Views/ar_view.dart';
@@ -15,7 +16,7 @@ class TreeViewInfoAr extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          const ARWidget(), //AR view widget
+          //const ARWidget(), //AR view widget
           DraggableScrollableSheet(
             //Bottom Sheet that show scanned tree info
             minChildSize: 0.10,
@@ -50,13 +51,16 @@ class TreeInfoSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String nameTree = tree.name;
+    final double pad = 10;
+    final double screenWidth = MediaQuery.of(context).size.width - (2 * pad);
+
     return Container(
       decoration: const BoxDecoration(
           borderRadius:
               BorderRadius.only(topLeft: radiusCorner, topRight: radiusCorner),
           color: secondColor),
       child: Padding(
-        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        padding: EdgeInsets.only(top: pad, left: pad, right: pad),
         child: ListView(controller: controller, children: [
           Text(nameTree,
               style: const TextStyle(
@@ -83,30 +87,47 @@ class TreeInfoSheet extends StatelessWidget {
               ),
             ),
           ),
-          Row(
-            children: [
-              Text("Co2: ${tree.co2}"),
-              getIconIndicator(StatsType.co2, 3),
-            ],
-          ),
-          Row(
-            children: [
-              Text("Altezza: ${tree.height}"),
-              getIconIndicator(StatsType.paper, 5),
-            ],
-          ),
-          Row(
-            children: [
-              Text("Larghezza: ${tree.diameter}"),
-              getIconIndicator(StatsType.paper, 9),
-            ],
-          ),
+          rowIndicator('Co2', StatsType.co2, tree.co2, 1000, screenWidth),
+          rowIndicator(
+              'Altezza (cm) ', StatsType.height, tree.height, 100, screenWidth),
+          rowIndicator('Tronco (cm)', StatsType.diameter, tree.diameter, 90,
+              screenWidth),
+          rowIndicator('Acqua', StatsType.water, 30, 300, screenWidth),
         ]),
       ),
     );
   }
 
-//TODO: get as param type of info (make an enum to define it)
+  Widget rowIndicator(
+    String label,
+    StatsType type,
+    int value,
+    int maxValue,
+    double screenWidth,
+  ) {
+    const double iconSizeDefault = 24;
+    const double textSize = 15;
+    String labelValue = "$label: $value";
+
+    if (value > maxValue) {
+      value = maxValue;
+    }
+
+    double textPixel = textSize * labelValue.length;
+    int maxIconCount = ((screenWidth - textPixel) / iconSizeDefault).ceil();
+    int mappedVal = (maxIconCount * value) ~/ maxValue + 1;
+
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(right: 5),
+          child: Text(labelValue, style: const TextStyle(fontSize: textSize)),
+        ),
+        getIconIndicator(type, mappedVal),
+      ],
+    );
+  }
+
   Widget getIconIndicator(StatsType type, int value) {
     return Row(
       children: <Icon>[
