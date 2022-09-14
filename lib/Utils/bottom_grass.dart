2b339b10c58b8c11abb3a60ee96c4ec22cf5e420
Matteo <matteo.violani@studio.unibo.it>
2022-09-14 15:52:29 +0200

    added widget that put grasson bottom

---
+++ lib/Utils/bottom_grass.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:tree_ar/constant_vars.dart';
+
+class BottomGrass extends StatelessWidget {
+  final Widget child;
+  const BottomGrass({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Flex(
+            direction: Axis.vertical,
+            children: [
+              Expanded(child: child),
+            ],
+          ),
+          LayoutBuilder(builder: ((context, constraints) {
+            var parentWidth = constraints.maxWidth;
+            return SizedBox(
+              width: parentWidth,
+              height: grassBottomBarHeight,
+              child: SvgPicture.asset(
+                '$imagePath/grass.svg',
+                color: mainColor,
+                excludeFromSemantics: true,
+                fit: BoxFit.fill,
+              ),
+            );
+          }))
+        ],
+      ),
+    );
+  }
+}
