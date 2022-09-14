ef6fa2b13f734dd399ce44844904d98bafbfc4b5
Matteo <matteo.violani@studio.unibo.it>
2022-09-14 17:05:04 +0200

    import code of list item

--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -4,6 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tree_ar/Database/data_model.dart';
+import 'package:tree_ar/Views/HomePage/home_list_item.dart';
 import 'package:tree_ar/Views/infoPageView.dart/info_item_page.dart';
 import 'package:tree_ar/utils.dart';
 
@@ -175,102 +176,5 @@ class _CustomListView extends State<CustomListView> {
         },
       );
     });
-  }
-}
-
-class RowItem extends StatelessWidget {
-  final ListItemInterface item;
-  final InfoType type;
-
-  static const margin5H = EdgeInsets.symmetric(horizontal: 5);
-
-  const RowItem({Key? key, required this.item, required this.type})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(5),
-      height: 85,
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(15)),
-        color: grayColor,
-      ),
-      child: Row(children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CachedNetworkImage(
-              imageUrl: item.getImageUrl()!,
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              height: 75,
-              width: 75,
-              errorWidget: (context, error, stackTrace) {
-                log(error.toString());
-                return ClipOval(
-                  child: Container(
-                    width: imageSizeDetailPage,
-                    height: imageSizeDetailPage,
-                    color: grayColor,
-                    child: Icon(
-                        type == InfoType.tree
-                            ? Icons.nature
-                            : Icons.construction,
-                        size: 50),
-                  ),
-                );
-              },
-            )
-            // Container(
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(10),
-            //     color: const Color.fromARGB(255, 217, 217, 217),
-            //   ),
-            //   margin: const EdgeInsets.symmetric(horizontal: 15),
-            //   height: 57,
-            //   width: 57,
-            //   child: item.getImageUrl(),
-            // )
-          ],
-        ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flex(
-                direction: Axis.vertical,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: Text(
-                      item.getTitle(),
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 17),
-                    ),
-                  ),
-                ],
-              ),
-              Flex(
-                direction: Axis.vertical,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: Text(
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      item.getDescr(),
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ]),
-    );
   }
 }
