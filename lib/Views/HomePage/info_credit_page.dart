9bb18ad37c7c7b2f502d78f2941c0a2e8a6eba54
Matteo <matteo.violani@studio.unibo.it>
2022-09-15 19:39:29 +0200

    info page empty

---
+++ lib/Views/HomePage/info_credit_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tree_ar/constant_vars.dart';
+
+class InfoCreditsPage extends StatelessWidget {
+  const InfoCreditsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Informazioni'),
+        backgroundColor: mainColor,
+      ),
+    );
+  }
+}
