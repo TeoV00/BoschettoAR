35a527c652d3941a8e707a2363669e2f92904baa
Matteo <matteo.violani@studio.unibo.it>
2022-09-18 15:38:48 +0200

    removed some comments

--- lib/Views/HomePage/InfoPage/info_page_const.dart
+++ lib/Views/HomePage/InfoPage/info_page_const.dart
@@ -12,9 +12,9 @@ final infoSections = [
         '''Fornire informazioni sulla sostenibilità dell’Università di Bologna rendendo più consapevoli gli studenti sul tema.''',
   },
   {
-    'title': '''Che obbiettivo ha questa app?''',
+    'title': '''Come è stata calcolata la co2 a partire dai fogli di carta?''',
     'body':
-        '''I dati della co2, energia elettrica e barili di petrolio sono stati calcolati a partire dalla quantità di fogli di carta risparmiati.
+        '''I dati della co2, energia elettrica e taniche di benzina (da 20 litri) sono stati calcolati a partire dalla quantità di fogli di carta risparmiati.
 Nei calcoli delle conversioni è stato considerato un foglio di carta da ufficio con densità pari a 80g/m².
 
 Considerato, come regola generale, che 1 Kg di carta corrisponde ad 1 Kg di Co2 durante la sua produzione (1,2 kg di CO2 per la carta non riciclata e 0,7 kg di CO2 per la carta riciclata) si ottiene che si hanno circa 16 fogli/m².
@@ -22,26 +22,13 @@ Considerato, come regola generale, che 1 Kg di carta corrisponde ad 1 Kg di Co2 
 Si ha quindi che un folgio di carta corrisponde a circa 5g di Co2.''',
   },
   {
-    'title': '''Come sono stati calcolati i litri di petrolio?''',
+    'title': '''Come sono stati calcolati i litri di benzina?''',
     'body':
-        '''Per calcolare i litri di petrolio corrispondendi ai Kg di Co2 si è moltiplicato il valore della Co2 risparmiata per la costante 0.429''',
+        '''Per calcolare i litri di benzina corrispondendi ai Kg di Co2 si è moltiplicato il valore della Co2 risparmiata per la costante 0.429''',
   },
   {
     'title': '''Come è stato calcolata l'energia elettrica?''',
-    'body': '''''',
+    'body':
+        '''Considerando un PCI medio di 43,6 MJ/kg, e una densità media di circa 750g/L. Quindi si ottiene 43,6 [MJ/kg] * 0,75 [kg/L] * 0,277 [kWh/MJ] ~= 9,06 kWh/L''',
   },
 ];
-
-// // x = 429 petrol litri/1000 Kg = 0,429 Litri Petrolio/Kg
-//   // from Kg Co2 --> petrol oil liters
-//   static const double literPerKgOil = 0.429;
-
-//   // //from petrol liters --> giga Joule energy --> GWh
-//   // const double gigJoulePerLiterOil = 0.03501030928;
-//   // const double gigWattHourPerGigJoule = 0.000277778;
-
-//   static const double gigWattHourPerLiterOil = 0.000009725093691;
-
-//   // barels/liter petrolio = 0,006293706294 Barrels/liter
-//   //from liters petrol oil --> petrol oil barrels
-//   static const double barrelsPerLiterOil = 0.006293706294;
