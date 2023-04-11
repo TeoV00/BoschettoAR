521453e96ed4e601bdb3086baf724a0d31c01ce0
Matteo <matteo.violani@studio.unibo.it>
2023-04-12 00:00:15 +0200

    fix(share_progress): minor fix

--- lib/Views/UploadProgress/share_progress.dart
+++ lib/Views/UploadProgress/share_progress.dart
@@ -7,6 +7,8 @@ import 'package:tree_ar/constant_vars.dart';
 
 const pageTitle = "Deposita progressi";
 const nickname = "TeoV00";
+const String scanGuide =
+    "Recati nel totem più vicino, clicca sul pulsante “Deposita Progressi APP” e scansiona il Qr code";
 
 class SharePorgressPage extends StatelessWidget {
   const SharePorgressPage({super.key});
@@ -16,100 +18,110 @@ class SharePorgressPage extends StatelessWidget {
     return Scaffold(
       appBar: getAppBar("Deposita progressi", context),
       body: BottomGrass(
-          child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              "In questa sezione potrai depositare i tuoi progressi per gareggiare contro gli altri e rendere il bosco virtuale rigoglioso!",
-              style: TextStyle(
-                fontSize: 18,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "In questa sezione potrai depositare i tuoi progressi per gareggiare contro gli altri e rendere il bosco virtuale rigoglioso!",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-            const Spacer(),
-            LayoutBuilder(
-              builder: (context, constraint) {
-                return Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color.fromRGBO(255, 247, 180, 1),
-                      boxShadow: const [
-                        BoxShadow(
-                            blurRadius: 4,
-                            offset: Offset(0, 4),
-                            spreadRadius: 0,
-                            color: Color.fromRGBO(0, 0, 0, 0.25))
-                      ]),
-                  width: constraint.maxWidth,
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                  child: Column(
-                    children: const [
-                      Text(
-                        "Dati condivisi",
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500, fontSize: 16),
+              const Spacer(),
+              const Text(
+                scanGuide,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                ),
+              ),
+              const Spacer(),
+              LayoutBuilder(
+                builder: (context, constraint) {
+                  return Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromRGBO(255, 247, 180, 1),
+                        boxShadow: const [
+                          BoxShadow(
+                              blurRadius: 4,
+                              offset: Offset(0, 4),
+                              spreadRadius: 0,
+                              color: Color.fromRGBO(0, 0, 0, 0.25))
+                        ]),
+                    width: constraint.maxWidth,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 20),
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Dati condivisi",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 16),
+                        ),
+                        BulletList([
+                          "nickname",
+                          "numero dei badge",
+                          "percentuale progresso",
+                          "contatori risparmi (co2, carta, elettricità, benzina)"
+                        ]),
+                        Text("I dati saranno condivisi con il nickname:"),
+                        Text(
+                          nickname,
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              // const Spacer(),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Column(
+                  children: [
+                    Image.asset("assets/images/growing_trees.png"),
+                    const Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        "Più alberi scannerizzi e badge acquisisci più il tuo albero sarà rigoglioso!!",
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
                       ),
-                      BulletList([
-                        "nickname",
-                        "numero dei badge",
-                        "percentuale progresso",
-                        "contatori risparmi (co2, carta, elettricità, benzina)"
-                      ]),
-                      Text("I dati saranno condivisi con il nickname:"),
-                      Text(
-                        nickname,
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              LayoutBuilder(builder: (context, constraint) {
+                return MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  minWidth: constraint.maxWidth,
+                  height: 50,
+                  textColor: Colors.white,
+                  color: mainColor,
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) {
+                      return const ScanTotemQR();
+                    }),
+                  ),
+                  child: const Text(
+                    "Procedi",
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 );
-              },
-            ),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Column(
-                children: [
-                  Image.asset("assets/images/growing_trees.png"),
-                  const Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(
-                      "Più alberi scannerizzi e badge acquisisci più il tuo albero sarà rigoglioso!!",
-                      style: TextStyle(fontSize: 16),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            LayoutBuilder(builder: (context, constraint) {
-              return MaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                minWidth: constraint.maxWidth,
-                height: 50,
-                textColor: Colors.white,
-                color: mainColor,
-                onPressed: () => Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) {
-                    return const ScanTotemQR();
-                  }),
-                ),
-                child: const Text(
-                  "Procedi",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-              );
-            }),
-            const Spacer(),
-          ],
+              }),
+              const Spacer(),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
