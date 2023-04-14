6e8d4b14eec92fcc44590c4ad384fdb602a891e0
Matteo <matteo.violani@studio.unibo.it>
2023-04-15 01:34:18 +0200

    feat(upload_completed): completed upload view

---
+++ lib/Views/UploadProgress/upload_completed.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:tree_ar/constant_vars.dart';
+import 'package:tree_ar/utils.dart';
+
+class CompletedUploadView extends StatelessWidget {
+  const CompletedUploadView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CenteredWidget(
+      widgetToCenter: Column(
+        children: [
+          SizedBox(
+            height: 80,
+            child: Image.asset('$imagePath/treeVase/5.png'),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Invio dati sul totem",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
